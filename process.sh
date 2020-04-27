5a7bb490de59a1d4a660856d6d74228df696236c
siabar <30yamak@gmail.com>
2020-04-27 15:54:21 +0200

    updating process.sh

--- process.sh
+++ process.sh
@@ -7,13 +7,14 @@ export ANN_VARIABLE=/ICTUSnet/data/ANN_VARIABLE
 export ANN_SECTION=/ICTUSnet/data/ANN_SECTION
 export ANN_FINAL=/ICTUSnet/data/ANN_FINAL
 
-echo "Running pipeline for detecting Variables"
+echo "Detecting Variables ..."
 java -Djava.library.path=/usr/local/share/freeling/APIs/java -cp $CTAKES_HOME/lib/*:$CTAKES_HOME/ctakes-SpaCTeS-4.0.1-SNAPSHOT.jar -Dlog4j.configuration=file:$CTAKES_HOME/config/log4j.xml -Xmx8g  org.apache.ctakes.spactes.pipeline.SpaCTeSBuilderRunner
 
-echo "Running HeaderDetector for detecting Secions"
+echo "Detecting Sections ..."
 export HEADERDETECTOR=/ICTUSnet/HeaderDetector
-python3 $HEADERDETECTOR/scripts/header_detector.py --set "$1" --data $TXT
+python3 $HEADERDETECTOR/scripts/header_detector.py --data $TXT
 
+echo "MErging Variables and Sections ..."
 export BRATMERGER=/ICTUSnet/BratMerger
-python3 $BRATMERGER/scripts/merger.py --set "$1" --data $ANN_SECTION
+python3 $BRATMERGER/scripts/merger.py --data $ANN_SECTION
 
