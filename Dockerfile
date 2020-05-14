e3237054d2165a38a65e0d51f4d318792b8865b3
siabar <30yamak@gmail.com>
2020-05-14 19:47:15 +0200

    adding resources of ctakes SpaCTeS project

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,22 @@
 FROM taln/freeling:latest
 
-# MAINTAINER Siamak Barzegar <siamak.barzegar@bsc.es>
 LABEL maintainer="siamak.barzegar@bsc.es"
 
-RUN wget http://temu.bsc.es/spactes-resources-bin.tar.gz && \
-    tar -xzvf spactes-resources-bin.tar.gz && \
-    mkdir -p /ICTUSnet/ctakes-SpaCTeS/ && \
-    mv spactes-resources-bin/* /ICTUSnet/ctakes-SpaCTeS/ && \
-    rm -rf spactes-resources-bin*
+ENV HOME_ICTUS ""
+
+RUN mkdir -p $HOME_ICTUS/ICTUSnet/ctakes-SpaCTeS
+
+COPY spactes-resources-bin/ $HOME_ICTUS/ICTUSnet/ctakes-SpaCTeS/ 
 
 RUN apt-get -qq update && \
     apt-get install -y python3-dev python3-pip python3-tk python3-lxml python3-six
 
 RUN pip3 install unidecode
 
-WORKDIR /ICTUSnet
+WORKDIR $HOME_ICTUS/ICTUSnet
 
 RUN git clone https://github.com/TeMU-BSC/EHR-HeaderDetector-AnnotationAnalyser HeaderDetector
 RUN git clone https://github.com/TeMU-BSC/BratMerger
 
-COPY process.sh /ICTUSnet/
-RUN chmod a+x /ICTUSnet/process.sh && cp /ICTUSnet/process.sh /bin/process.sh
+COPY process.sh $HOME_ICTUS/ICTUSnet/
+RUN chmod a+x $HOME_ICTUS/ICTUSnet/process.sh && cp $HOME_ICTUS/ICTUSnet/process.sh /bin/process.sh
--- process.sh
+++ process.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
+export HOME_ICTUS=""
 
-export CTAKES_HOME=/ICTUSnet/ctakes-SpaCTeS
-
-export TXT=/ICTUSnet/data/TXT
-export ANN_VARIABLE=/ICTUSnet/data/ANN_VARIABLE
-export ANN_SECTION=/ICTUSnet/data/ANN_SECTION
-export ANN_FINAL=/ICTUSnet/data/ANN_FINAL
-
+export CTAKES_HOME=$HOME_ICTUS/ICTUSnet/ctakes-SpaCTeS
+export TXT=$HOME_ICTUS/ICTUSnet/data/TXT
+export ANN_VARIABLE=$HOME_ICTUS/ICTUSnet/data/ANN_VARIABLE
+export ANN_SECTION=$HOME_ICTUS/ICTUSnet/data/ANN_SECTION
+export ANN_FINAL=$HOME_ICTUS/ICTUSnet/data/ANN_FINAL
+export HEADERDETECTOR=$HOME_ICTUS/ICTUSnet/HeaderDetector
+export BRATMERGER=$HOME_ICTUS/ICTUSnet/BratMerger
 echo "Detecting Variables ..."
-java -Djava.library.path=/usr/local/share/freeling/APIs/java -cp $CTAKES_HOME/lib/*:$CTAKES_HOME/ctakes-SpaCTeS-4.0.1-SNAPSHOT.jar -Dlog4j.configuration=file:$CTAKES_HOME/config/log4j.xml -Xmx8g  org.apache.ctakes.spactes.pipeline.SpaCTeSBuilderRunner
+java -Djava.library.path=/usr/local/share/freeling/APIs/java -cp $CTAKES_HOME/lib/*:$CTAKES_HOME/ctakes-SpaCTeS-4.0.1-SNAPSHOT.jar -Dlog4j.configuration=file:$CTAKES_HOME/config/log4j.xml -Xmx8g  org.apache.ctakes.spactes.pipeline.SpaCTeSBuilderRunner $TXT $ANN_VARIABLE
 
 echo "Detecting Sections ..."
-export HEADERDETECTOR=/ICTUSnet/HeaderDetector
 python3 $HEADERDETECTOR/scripts/header_detector.py --data $TXT
 
 echo "Merging Variables and Sections ..."
-export BRATMERGER=/ICTUSnet/BratMerger
 python3 $BRATMERGER/script/merger.py --data $ANN_SECTION
 
